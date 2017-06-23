4da5583e2b37bbce08fbd50da4f74731182896d3
WNjihia <dorisn172@gmail.com>
2017-06-23 13:45:36 +0300

    [Feature #147322077] refactor default database

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -31,20 +31,22 @@ For a full list of options, visit:
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
-DATABASES = {
-    'default': {
-        'ENGINE': '',
-        'NAME': '',
-        'USER': '',
-        'PORT': '5432',
-        'TEST': {
-            'CHARSET': 'UTF8'
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': '',
+#         'NAME': '',
+#         'USER': '',
+#         'PORT': '5432',
+#         'TEST': {
+#             'CHARSET': 'UTF8'
+#         }
+#     }
+# }
+#
+# environment_db = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(environment_db)
 
-environment_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(environment_db)
+DATABASES = {'default' : dj_database_url.config() }
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
