495b7c4d55c48429aaef946ba5ebece5d7aebf3d
WNjihia <dorisn172@gmail.com>
2017-06-23 14:09:56 +0300

    [Feature #147322077] add default host

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -31,22 +31,23 @@ For a full list of options, visit:
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': '',
-#         'NAME': '',
-#         'USER': '',
-#         'PORT': '5432',
-#         'TEST': {
-#             'CHARSET': 'UTF8'
-#         }
-#     }
-# }
-#
-# environment_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(environment_db)
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST': {
+            'CHARSET': 'UTF8'
+        }
+    }
+}
 
-DATABASES = {'default' : dj_database_url.config(default=os.environ["DATABASE_URL"]) }
+environment_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(environment_db)
+
+# DATABASES = {'default' : dj_database_url.config(default=os.environ["DATABASE_URL"]) }
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
