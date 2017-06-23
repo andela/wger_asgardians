0b0b08ef6e32fc3071099c459931134e6638255e
WNjihia <dorisn172@gmail.com>
2017-06-23 14:26:55 +0300

    [Feature #147322077] update settings_global

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -19,6 +19,7 @@
 import os
 import re
 import sys
+import urlparse
 import dj_database_url
 
 
@@ -43,11 +44,47 @@ DATABASES = {
         }
     }
 }
-
-environment_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(environment_db)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+#
+# environment_db = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(environment_db)
 
 # DATABASES = {'default' : dj_database_url.config(default=os.environ["DATABASE_URL"]) }
+
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
+
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
