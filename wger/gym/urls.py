b542e28b0f216c0c9be892c78a46e97b3c5219cc
JacksonO <jackson.onyango@andela.com>
2017-07-05 10:40:14 +0300

    [ft #147322097]Add url pattern for members comparison overview

--- wger/gym/urls.py
+++ wger/gym/urls.py
@@ -69,6 +69,9 @@ patterns_gym = [
     url(r'^user/(?P<user_pk>\d+)/reset-user-password$',
         gym.reset_user_password,
         name='reset-user-password'),
+    url(r'^(?P<pk>\d+)/members-comparison$',
+        gym.GymMemberComparisonView.as_view(),
+        name='member-comparison'),
 ]
 
 # 'sub patterns' for gym config
